3ad648da6308d1370b74ef38597c8bd6aa1498df
Vũ Trung Tiến <22dh113737@st.huflit.edu.vn>
2026-07-01 22:14:28 +0700

    Initial Commit

---
+++ CindyHandmade/CindyHandmadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CindyHandmadeApp.swift
+//  CindyHandmade
+//
+//  Created by TrungTien on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CindyHandmadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
